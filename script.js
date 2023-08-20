6710c3d878759f17c93db958b2e5dd8b52cc43b5
Sangram <sangram1677@gmail.com>
2023-08-20 19:49:01 +0530

    getElementById

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Dom - Document object model
 // It is like tree like structure
 // It convert html code into js object
+
+// getElementByid() - the method return an element with specified id
+// it returns a single object
+
+console.log(document.getElementById('heading1'));
+console.log(document.getElementById('')); // return null
