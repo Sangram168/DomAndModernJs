1b5aed242a2f55737c00cd0954b137d8f3c808b9
Sangram <sangram1677@gmail.com>
2023-08-26 03:22:00 +0530

    Avoid to many events

--- script.js
+++ script.js
@@ -246,7 +246,50 @@ lin.addEventListener('click', function(event){
 })
 
 
+// Avoid to many event
 
+let myDiv = document.createElement('div');
+for(let i = 1; i<=100; i++){
+    let newElement = document.createElement('p');
+    newElement.textContent = 'This is para' + i;
+
+    newElement.addEventListener('click', function(event){
+        console.log('I have clicked a para');
+    });
+
+    myDiv.appendChild(newElement);
+}
+
+document.body.appendChild(myDiv);
+
+
+// More optimize
+// target - The target property returns the element where the event occured
+let myDiv2 = document.createElement('div');
+  function paraStatus(event){
+     console.log('Para' + event.target.textContent);
+ }
+for(let i = 1; i<=100; i++){
+    let newElement = document.createElement('p');
+    newElement.textContent = 'This is para' + i;
+
+    newElement.addEventListener('click', paraStatus);
+
+    myDiv2.appendChild(newElement);
+}
+
+document.body.appendChild(myDiv);
+
+
+//
+let element = document.querySelector('#wrapper');
+element.addEventListener('click', function(event){
+    if(event.target.nodeName === 'SPAN'){
+        console.log('span pr click kia hai' + event.target.textContent);
+
+    }
+    
+});
 
 
 
